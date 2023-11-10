b1616188a9591aa318d3dfa1a14bfa8a2162553a
Tzu Yi Chang <63236075+tzuyi126@users.noreply.github.com>
2023-11-11 05:40:21 +0800

    Create NumIslands.java

---
+++ DFS_BFS_BST/NumIslands.java
@@ -0,0 +1,33 @@
+// 200. Number of Islands
+// https://leetcode.com/problems/number-of-islands/
+class Solution {
+    public int numIslands(char[][] grid) {
+        int count = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    visitIsland(grid, i, j);
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private void visitIsland(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length) {
+            return;
+        }
+
+        if (grid[i][j] == '0') return;
+        
+        // Turn land into water
+        grid[i][j] = '0';
+        visitIsland(grid, i - 1, j);
+        visitIsland(grid, i + 1, j);
+        visitIsland(grid, i, j - 1);
+        visitIsland(grid, i, j + 1);
+    }
+}
