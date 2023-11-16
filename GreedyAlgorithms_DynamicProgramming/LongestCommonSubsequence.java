b2aa0a4b064605a808cb52794ce32fb13cb5eaa5
Tzu Yi Chang <63236075+tzuyi126@users.noreply.github.com>
2023-11-17 05:24:12 +0800

    Create LongestCommonSubsequence.java

---
+++ GreedyAlgorithms_DynamicProgramming/LongestCommonSubsequence.java
@@ -0,0 +1,32 @@
+// 1143. Longest Common Subsequence
+// https://leetcode.com/problems/longest-common-subsequence/
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int len1 = text1.length();
+        int len2 = text2.length();
+
+        int[][] dp = new int[len1 + 1][len2 + 1];
+
+        for (int i = 0; i < len1 + 1; i++) {
+            for (int j = 0; j < len2 + 1; j++) {
+                if (i == 0) {
+                    dp[0][j] = 0;
+                    continue;
+                }
+
+                if (j == 0) {
+                    dp[i][0] = 0;
+                    continue;
+                }
+
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[len1][len2];
+    }
+}
